0412569a5f66aabcc969f3d0850a1fbb0d3340c2
Aravind Dasarathy <dasarathy.ar@northeastern.edu>
2023-02-04 19:32:13 -0500

    assign part 1

--- src/main/java/edu/neu/coe/info6205/util/Timer.java
+++ src/main/java/edu/neu/coe/info6205/util/Timer.java
@@ -1,5 +1,6 @@
 package edu.neu.coe.info6205.util;
 
+import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Supplier;
@@ -61,7 +62,32 @@ public class Timer {
     public <T, U> double repeat(int n, Supplier<T> supplier, Function<T, U> function, UnaryOperator<T> preFunction, Consumer<U> postFunction) {
         logger.trace("repeat: with " + n + " runs");
         // FIXME: note that the timer is running when this method is called and should still be running when it returns. by replacing the following code
-         return 0;
+
+        pause();
+
+        for (int i = 0; i < n; i++) {
+            T inputArray = supplier.get();
+
+            if (preFunction != null) {
+                inputArray = preFunction.apply(inputArray);
+            }
+
+            resume();
+
+            U result = function.apply(inputArray);
+
+            pauseAndLap();
+
+            if (postFunction != null) {
+                postFunction.accept(result);
+            }
+        }
+
+        final double averageTime = meanLapTime();
+
+        resume();
+
+        return averageTime;
         // END 
     }
 
@@ -188,7 +214,7 @@ public class Timer {
      */
     private static long getClock() {
         // FIXME by replacing the following code
-         return 0;
+        return System.nanoTime();
         // END 
     }
 
@@ -201,8 +227,8 @@ public class Timer {
      */
     private static double toMillisecs(long ticks) {
         // FIXME by replacing the following code
-         return 0;
-        // END 
+        return TimeUnit.NANOSECONDS.toMillis(ticks);
+        // END
     }
 
     final static LazyLogger logger = new LazyLogger(Timer.class);
